f48c8d6ff06e5bc00283994e7846ba6e3d3cbfe6
KashishTheCoder <kashishbasreja.fss22@gmail.com>
2024-06-29 19:13:08 +0000

    Pending changes exported from your codespace

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,37 @@
 // Write your code here
+class Book{
+  constructor(title, author, ISBN, numCopies){
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+  getAvailability(){
+    if(this.numCopies === 0)
+      return "Out of Stock";
+    else if (this.numCopies < 10)
+      return "Low Stock";
+    else
+      return "In Stock";
+  }
+  get availability(){
+    return this.getAvailability();
+  }
+  sell(numSold = 1){
+      this.numCopies -= numSold;
+  }
+  restock(numCopies = 5){
+    this.numCopies += numCopies;
+  }
+}
+
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log("Book Stock:", HungerGames.availability);
+
+HungerGames.restock(12);
+console.log("12 Books Restocked!");
+console.log("Book Stock:", HungerGames.availability);
+
+HungerGames.sell(17);
+console.log("17 Books Sold!");
+console.log("Book Stock:", HungerGames.availability);
--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,9 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+const user = {
+  [username] : "kashish",
+  [password] : "kashish"
+}; 
+console.log(user.username);
+console.log(user.password);
